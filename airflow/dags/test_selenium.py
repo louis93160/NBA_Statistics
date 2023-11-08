9383fe4b8b80314908e17fb77c68870581812823
Oliver Giles <oliver.giles@lewagon.org>
2023-11-08 13:31:41 +0000

    updated extracted dag

--- airflow/dags/test_selenium.py
+++ airflow/dags/test_selenium.py
@@ -12,6 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 
 
 
@@ -77,71 +78,145 @@ def stathead_extraction():
 
 
         # #############################################################
-        # # Navigate to the TEAM GAME REBOUNDS finder page
-        # driver.get("https://stathead.com/basketball/team-game-finder.cgi?request=1&order_by=trb&year_min=2024&year_max=2024&game_month=11&game_day=5")
+        # Navigate to the TEAM GAME REBOUNDS finder page
+        driver.get("https://stathead.com/basketball/team-game-finder.cgi?request=1&order_by=trb&year_min=2024&year_max=2024&game_month=11&game_day=5")
 
-        # # Wait for the next page to load or for a confirmation of login
-        # time.sleep(5)
-        # # print("Navigated to team game finder page")
-        # # print(driver.page_source)
+        # Wait for the next page to load or for a confirmation of login
+        time.sleep(5)
+        # print("Navigated to team game finder page")
+        # print(driver.page_source)
 
-        # stats_table_xpath = "//table[contains(@class, 'stats_table')]"
-        # # print(stats_table_xpath)
-        # # print("Stats table")
-        # print(driver.find_element(By.XPATH, stats_table_xpath).text)
+        stats_table_xpath = "//table[contains(@class, 'stats_table')]"
+        # print(stats_table_xpath)
+        # print("Stats table")
+        game_rebounds_content = driver.find_element(By.XPATH, stats_table_xpath).text
 
         # #############################################################
         # # Navigate to the TEAM GAME ASSISTS finder page
-        # driver.get("https://stathead.com/basketball/team-game-finder.cgi?request=1&order_by=ast&year_min=2024&year_max=2024&game_month=11&game_day=5")
+        driver.get("https://stathead.com/basketball/team-game-finder.cgi?request=1&order_by=ast&year_min=2024&year_max=2024&game_month=11&game_day=5")
 
-        # # Wait for the next page to load or for a confirmation of login
-        # time.sleep(5)
-        # # print("Navigated to team game finder page")
-        # # print(driver.page_source)
+        # Wait for the next page to load or for a confirmation of login
+        time.sleep(5)
+        # print("Navigated to team game finder page")
+        # print(driver.page_source)
 
-        # stats_table_xpath = "//table[contains(@class, 'stats_table')]"
-        # # print(stats_table_xpath)
-        # # print("Stats table")
-        # print(driver.find_element(By.XPATH, stats_table_xpath).text)
-
-        # #############################################################
-        # # Navigate to the PLAYER GAME LOG FIRST 200 RESULTS finder page
-        # driver.get("https://stathead.com/basketball/player-game-finder.cgi?request=1&order_by=date&year_min=2024&year_max=2024&game_month=10&game_day=25")
-
-        # # Wait for the next page to load or for a confirmation of login
-        # time.sleep(5)
-        # # print("Navigated to team game finder page")
-        # # print(driver.page_source)
-
-        # stats_table_xpath = "//table[contains(@class, 'stats_table')]"
-        # # print(stats_table_xpath)
-        # # print("Stats table")
-        # print(driver.find_element(By.XPATH, stats_table_xpath).text)
+        stats_table_xpath = "//table[contains(@class, 'stats_table')]"
+        # print(stats_table_xpath)
+        # print("Stats table")
+        game_assists_content = driver.find_element(By.XPATH, stats_table_xpath).text
 
         # #############################################################
-        # # Navigate to the PLAYER GAME LOG NEXT 200 RESULTS finder page
-        # driver.get("https://stathead.com/basketball/player-game-finder.cgi?request=1&order_by=date&year_min=2024&year_max=2024&game_month=10&game_day=25&offset=200")
+        # Navigate to the PLAYER GAME LOG FIRST 200 RESULTS finder page
+        driver.get("https://stathead.com/basketball/player-game-finder.cgi?request=1&order_by=date&year_min=2024&year_max=2024&game_month=10&game_day=25")
 
-        # # Wait for the next page to load or for a confirmation of login
-        # time.sleep(5)
-        # # print("Navigated to team game finder page")
-        # # print(driver.page_source)
+        # Wait for the next page to load or for a confirmation of login
+        time.sleep(5)
+        # print("Navigated to team game finder page")
+        # print(driver.page_source)
 
-        # stats_table_xpath = "//table[contains(@class, 'stats_table')]"
-        # # print(stats_table_xpath)
-        # # print("Stats table")
-        # print(driver.find_element(By.XPATH, stats_table_xpath).text)
+        stats_table_xpath = "//table[contains(@class, 'stats_table')]"
+        # print(stats_table_xpath)
+        # print("Stats table")
+        players_content_200 = driver.find_element(By.XPATH, stats_table_xpath).text
+
+        # #############################################################
+        # Navigate to the PLAYER GAME LOG NEXT 200 RESULTS finder page
+        driver.get("https://stathead.com/basketball/player-game-finder.cgi?request=1&order_by=date&year_min=2024&year_max=2024&game_month=10&game_day=25&offset=200")
+
+        # Wait for the next page to load or for a confirmation of login
+        time.sleep(5)
+        # print("Navigated to team game finder page")
+        # print(driver.page_source)
+
+        stats_table_xpath = "//table[contains(@class, 'stats_table')]"
+        # print(stats_table_xpath)
+        # print("Stats table")
+        players_content_400 = driver.find_element(By.XPATH, stats_table_xpath).text
 
         # Don't forget to close the browser
         driver.quit()
-        return game_points_content
+        return game_points_content, game_rebounds_content, game_assists_content, players_content_200, players_content_400
 
     @task()
-    def print_content(game_points_content):
+    def extract_points_content(result):
+        game_points_content = result[0]
+        print("Game points content")
         print(game_points_content)
+        result = [line for line in game_points_content.split('\n') if line != '']
+
+        headers = result[1].split(' ')
+        games = result[2:]
+
+        games = [game.split() for game in games]
+        for game in games:
+            if "@" in game:
+                game.remove("@")
+            if "(OT)" in game:
+                game.remove("(OT)")
+            game[5] += " " + game.pop(6)
+
+        return games, headers
+
+    @task()
+    def extract_games_rebounds_content(result):
+        game_rebounds_content = result[1]
+        print("Game rebound content")
+        print(game_rebounds_content)
+        result = [line for line in game_rebounds_content.split('\n') if line != '']
+
+        headers = result[1].split(' ')
+        games = result[2:]
+
+        games = [game.split() for game in games]
+        for game in games:
+            if "@" in game:
+                game.remove("@")
+            if "(OT)" in game:
+                game.remove("(OT)")
+            game[5] += " " + game.pop(6)
+
+        return games, headers
+
+    @task()
+    def extract_games_assists_content(result):
+        game_assists_content = result[2]
+        print("Game assists content")
+        print(game_assists_content)
+        result = [line for line in game_assists_content.split('\n') if line != '']
+
+        headers = result[1].split(' ')
+        games = result[2:]
+
+        games = [game.split() for game in games]
+        for game in games:
+            if "@" in game:
+                game.remove("@")
+            if "(OT)" in game:
+                game.remove("(OT)")
+            game[5] += " " + game.pop(6)
+
+        return games, headers
+
+    @task()
+    def extract_games_players_content(result):
+        print(result[3])
+        print(result[4])
+
+    @task()
+    def combine_games_content(extracted_game_points_content, extract_games_rebounds_content, extracted_game_assists_content):
+        point_frame = pd.DataFrame(extracted_game_points_content[0], columns=extracted_game_points_content[1])
+        rebound_frame = pd.DataFrame(extract_games_rebounds_content[0], columns=extract_games_rebounds_content[1])
+        assist_frame = pd.DataFrame(extracted_game_assists_content[0], columns=extracted_game_assists_content[1])
+        print(point_frame)
+        print(rebound_frame)
+        print(assist_frame)
 
     result = scraper()
-    print_content(result)
+    extracted_game_points_content = extract_points_content(result)
+    extract_games_rebounds_content = extract_games_rebounds_content(result)
+    extracted_game_assists_content = extract_games_assists_content(result)
+    extract_games_players_content = extract_games_players_content(result)
+    combine_games_content(extracted_game_points_content, extract_games_rebounds_content, extracted_game_assists_content)
 
 
 extraction = stathead_extraction()
