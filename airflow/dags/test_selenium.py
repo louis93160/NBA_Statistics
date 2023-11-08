27924d71246af893bc73c1bfe1367fe800abd726
Steph Ravoninjatovo <hsravo1@gmail.com>
2023-11-08 09:47:55 +0000

    adding dbt dag

--- airflow/dags/test_selenium.py
+++ airflow/dags/test_selenium.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+import time
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,20 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-
-import pandas as pd
-import csv
 
 
 def test_selenium_driver():
